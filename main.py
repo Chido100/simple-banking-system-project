54132e48e124bc368d16dd173ead536a3b7a3766
Chido100 <chido_dozie@hotmail.com>
2024-10-21 11:29:20 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+class BankAccount:
+    def __init__(self, account_number, account_holder, initial_balance=0):
+        self.account_number = account_number
+        self.account_holder = account_holder
+        self.balance = initial_balance
+        self.transactions = []
+
+        
