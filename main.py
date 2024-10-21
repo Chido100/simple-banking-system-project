8c8d88471667a47d889607d2ed3dccc9d70b9f51
Chido100 <chido_dozie@hotmail.com>
2024-10-21 12:33:58 +0100

    Added deposit and withdrawal functions

--- main.py
+++ main.py
@@ -8,3 +8,33 @@ class BankAccount:
     def __str__(self):
         return f"Account[{self.account_number}] - Holder: {self.account_holder} - Balance: £{self.balance}"
 
+
+    # Deposit into account
+    def deposit(self, amount: float):
+        if ammount > 0:
+            self.balance += amount
+            self.transactions.append(f"Deposited: £{amount}")
+            print(f"{amount} deposited successfully.")
+        else:
+            print("Deposit amount must be positive.")
+    
+    # Withdraw from account
+    def withdraw(self, amount: float):
+        if amount > 0:
+            if amount <= self.balance:
+                self.balance -= amount
+                self.transactions.append(f"Withdrew: £{amount}")
+                print(f"£{amount} withdrawn successfully.")
+            else:
+                print("Insufficient funds.")
+        else:
+            print("Withdrawal amount must be positive")
+
+    
+    def check_balance(self):
+        print(f"Current balance: £{self.balance}")
+        return self.balance
+
+
+    
+
